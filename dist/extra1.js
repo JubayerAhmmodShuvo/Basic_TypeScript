5750a9e382b8adb30c06ac2ce7b11ecc6d6bda12
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-05-08 00:26:49 +0600

    extra 1 done

---
+++ dist/extra1.js
@@ -0,0 +1,9 @@
+"use strict";
+const names = ["Shakib", "Mehedi", "Sayed"];
+const data = [69, "Bye"];
+names.push("Jubayer");
+names.push("Ahmmod");
+data[0] = 96;
+data[1] = "Love";
+console.log(names);
+console.log(data);
---
+++ src/extra1.ts
@@ -0,0 +1,12 @@
+const names: string[] = ["Shakib", "Mehedi", "Sayed"];
+
+const data: [number, string] = [69, "Bye"];
+
+names.push("Jubayer");
+names.push("Ahmmod");
+
+data[0] = 96;
+data[1] = "Love";
+
+console.log(names);
+console.log(data);
