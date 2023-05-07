348f4e2881823ba9c8fed6fdea2025cea6d2dd1d
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-05-07 22:20:27 +0600

    first Problem Solved

---
+++ dist/problem_1.js
@@ -0,0 +1,6 @@
+"use strict";
+function repeatedString(str, num = 3) {
+    console.log(str.repeat(num));
+}
+repeatedString("Jubayer", 6); //calling function with both parameter
+repeatedString("Shuvo"); //calling function with one parameter
---
+++ src/problem_1.ts
@@ -0,0 +1,7 @@
+function repeatedString(str: string, num: number=3) {
+  console.log(str.repeat(num));
+}
+
+repeatedString("Jubayer", 6);  //calling function with both parameter
+
+repeatedString("Shuvo");  //calling function with one parameter
